fbe56d7ff76e70fb197a08e3ff7c3c2b503e96b1
Nora Söderlund <nora.soderlund@polestar.com>
2023-09-24 20:39:05 +0200

    Update Sweden asset

--- .github/scripts/render/src/geojson.ts
+++ .github/scripts/render/src/geojson.ts
@@ -3,39 +3,8 @@ import TerrainTiles from "../../../../src/core/terrain/TerrainTiles";
 import TerrainCanvas from "../../../../src/core/terrain/TerrainCanvas";
 import NodeTerrainGrid from "../../../../src/node/terrain/NodeTerrainGrid";
 
-const swedenAssets = [
-    "Sweden_Blekinge.json",
-    "Sweden_Dalarna.json",
-    "Sweden_Gävleborg.json",
-    "Sweden_Gotland.json",
-    "Sweden_Halland.json",
-    "Sweden_Jämtland.json",
-    "Sweden_Jönköping.json",
-    "Sweden_Kalmar.json",
-    "Sweden_Kronoberg.json",
-    "Sweden_Norrbotten.json",
-    "Sweden_Orebro.json",
-    "Sweden_Östergötland.json",
-    "Sweden_Skåne.json",
-    "Sweden_Södermanland.json",
-    "Sweden_Stockholm.json",
-    "Sweden_Uppsala.json",
-    "Sweden_Värmland.json",
-    "Sweden_Västerbotten.json",
-    "Sweden_Västernorrland.json",
-    "Sweden_Västmanland.json",
-    "Sweden_Västra_Götaland.json"
-];
-
-const terrainGrids = swedenAssets.map((asset) => {
-    return NodeTerrainGrid.fromAsset("../../../assets/sweden/" + asset);
-});
-
-const terrainTilesCollection = terrainGrids.map((terrainGrid) => {
-    const terrainTiles = new TerrainTiles(terrainGrid);
-
-    return terrainTiles;
-});
+const terrainGrid = await NodeTerrainGrid.fromAsset("../../../../assets/Sweden.json");
+const terrainTilesCollection = [ new TerrainTiles(terrainGrid) ];
 
 const terrainCanvas = new TerrainCanvas(terrainTilesCollection, 10);
 
--- src/index.ts
+++ src/index.ts
@@ -18,39 +18,9 @@ const testTerrainGrid = new TerrainGrid([
 
 
 (async () => {
-  const swedenAssets = [
-    "Sweden_Blekinge.json",
-    "Sweden_Dalarna.json",
-    "Sweden_Gävleborg.json",
-    "Sweden_Gotland.json",
-    "Sweden_Halland.json",
-    "Sweden_Jämtland.json",
-    "Sweden_Jönköping.json",
-    "Sweden_Kalmar.json",
-    "Sweden_Kronoberg.json",
-    "Sweden_Norrbotten.json",
-    "Sweden_Orebro.json",
-    "Sweden_Östergötland.json",
-    "Sweden_Skåne.json",
-    "Sweden_Södermanland.json",
-    "Sweden_Stockholm.json",
-    "Sweden_Uppsala.json",
-    "Sweden_Värmland.json",
-    "Sweden_Västerbotten.json",
-    "Sweden_Västernorrland.json",
-    "Sweden_Västmanland.json",
-    "Sweden_Västra_Götaland.json"
-  ];
 
-  const terrainGrids = (await Promise.all(swedenAssets.map((asset) => {
-    return BrowserTerrainGrid.fromAsset("../assets/sweden/" + asset);
-  })));
-  
-  const terrainTilesCollection = terrainGrids.map((terrainGrid) => {
-    const terrainTiles = new TerrainTiles(terrainGrid);
-
-    return terrainTiles;
-  });
+  const terrainGrid = await BrowserTerrainGrid.fromAsset("../assets/Sweden.json");
+  const terrainTilesCollection = [ new TerrainTiles(terrainGrid) ];
 
   const testTerrainTiles = new TerrainTiles(testTerrainGrid);
   const testTerrainTilesCollection = [ testTerrainTiles ];
